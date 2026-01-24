3630359c84c433804e23c741fdc3c9a5af3eec78
Michael Cuomo <mcuomo777@gmail.com>
2026-01-23 21:10:15 -0500

    update packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,16 +8,41 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 install -y btop 
+dnf5 install -y clang
+dnf5 install -y fzf # cli fuzzy find
+dnf5 install -y git
+dnf5 install -y libfido2
+dnf5 install -y neovim python3-neovim
+dnf5 install -y ripgrep
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# might need to use terrapkg - see: https://github.com/terrapkg/packages/tree/frawhide
+# dnf5 install -y zed
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# flatpak?
+# dnf5 install -y obsidian
+
+
+# COPR
+
+# Enable and Install
+dnf5 -y copr enable scottames/ghostty
+dnf5 install -y ghostty
+
+dnf5 -y copr enable dejan/lazygit
+dnf5 install -y lazygit
+
+dnf5 -y copr enable sneexy/zen-browser
+dnf5 install -y zen-browser
+
+dnf5 -y copr enable varlad/zellij
+dnf5 install -y zellij
+
+# Disable
+dnf5 -y copr disable scottames/ghostty
+dnf5 -y copr disable dejan/lazygit
+dnf5 -y copr disable sneexy/zen-browser
+dnf5 -y copr disable carlad/zellij
 
 #### Example for enabling a System Unit File
 
