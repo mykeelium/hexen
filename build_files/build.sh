d5ed0488f582c49f90d3962086deded74fc24022
Michael Cuomo <mcuomo777@gmail.com>
2026-01-24 01:08:56 -0500

    fix: adding back the dnf5

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,13 +8,13 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-dnf install -y btop 
-dnf install -y clang
-dnf install -y fzf # cli fuzzy find
-dnf install -y git
-dnf install -y libfido2
-dnf install -y neovim python3-neovim
-dnf install -y ripgrep
+dnf5 install -y btop 
+dnf5 install -y clang
+dnf5 install -y fzf # cli fuzzy find
+dnf5 install -y git
+dnf5 install -y libfido2
+dnf5 install -y neovim python3-neovim
+dnf5 install -y ripgrep
 
 # might need to use terrapkg - see: https://github.com/terrapkg/packages/tree/frawhide
 # dnf5 install -y zed
@@ -26,23 +26,23 @@ dnf install -y ripgrep
 # COPR
 
 # Enable and Install
-dnf -y copr enable scottames/ghostty
-dnf install -y ghostty
+dnf5 -y copr enable scottames/ghostty
+dnf5 install -y ghostty
 
-dnf -y copr enable dejan/lazygit
-dnf install -y lazygit
+dnf5 -y copr enable dejan/lazygit
+dnf5 install -y lazygit
 
 # dnf5 -y copr enable sneexy/zen-browser
 # dnf5 install -y zen-browser
 
-dnf -y copr enable varlad/zellij
-dnf install -y zellij
+dnf5 -y copr enable varlad/zellij
+dnf5 install -y zellij
 
 # Disable
-dnf -y copr disable scottames/ghostty
-dnf -y copr disable dejan/lazygit
+dnf5 -y copr disable scottames/ghostty
+dnf5 -y copr disable dejan/lazygit
 # dnf5 -y copr disable sneexy/zen-browser
-dnf -y copr disable varlad/zellij
+dnf5 -y copr disable varlad/zellij
 
 #### Example for enabling a System Unit File
 
